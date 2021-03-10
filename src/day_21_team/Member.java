a2176d6d4b253ee03e51069a04bc445d1cc7f3a9
BDM <ehdalseorka2@gmail.com>
2021-03-10 23:28:20 +0900

    인사관리 구현 완료

--- src/day_21_team/Member.java
+++ src/day_21_team/Member.java
@@ -6,6 +6,8 @@ public class Member implements interface_01{
 	private Scanner input = new Scanner(System.in);
 	private int num;
 	private boolean roop;
+	Member_DB m_db = new Member_DB();
+	Member_Data m = new Member_Data();
 
 	@Override
 	public void display() {
@@ -16,9 +18,11 @@ public class Member implements interface_01{
 			System.out.println("1.확인 2.등록 3.수정 4.삭제 5.뒤로");
 			System.out.print(">>> ");
 			num = input.nextInt();
-			
+
 			switch(num) {
-			case 1: this.show(); break;
+			case 1: 
+				this.show();
+				break;
 			case 2: this.add(); break;
 			case 3: this.modify(); break;
 			case 4: this.delete(); break;
@@ -29,26 +33,71 @@ public class Member implements interface_01{
 
 	@Override
 	public void show() {
+		m_db.getMember();
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void add() {
-		// TODO Auto-generated method stub
+		System.out.print("사원번호 입력 : ");
+		m.setSerial(input.next());
+		System.out.print("이름 입력 : ");
+		m.setName(input.next());
+		System.out.print("부서 입력 : ");
+		m.setDepartment(input.next());
+		System.out.print("직책 입력 : ");
+		m.setPosition(input.next());
+		System.out.print("입사년도 입력 : ");
+		m.setYear(input.nextInt());
 		
+		m_db.saveMember(m);
+		System.out.println("등록되었습니다.");
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
 	public void modify() {
-		// TODO Auto-generated method stub
+		System.out.print("수정 사원번호 입력 : ");
+		m.setSerial(input.next());
+		System.out.print("수정 이름 입력 : ");
+		m.setName(input.next());
+		System.out.print("수정 부서 입력 : ");
+		m.setDepartment(input.next());
+		System.out.print("수정 직책 입력 : ");
+		m.setPosition(input.next());
+		System.out.print("수정 입사년도 입력 : ");
+		m.setYear(input.nextInt());
 		
+		int result = m_db.modifyMember(m);
+		if(result == 1) {
+			System.out.println("수정되었습니다.");
+		}
+		else {
+			System.out.println("수정 할 수 없습니다");
+		}
+		
+		
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
 	public void delete() {
-		// TODO Auto-generated method stub
+		System.out.print("삭제 할 사원번호 입력 : ");
+		String serial = input.next();
+		int result = m_db.deleteMember(serial);
 		
+		if(result == 1) {
+			System.out.println("삭제되었습니다.");
+		}
+		else {
+			System.out.println("삭제 할 수 없습니다.");
+		}
+		
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
--- src/day_21_team/Member_Data.java
+++ src/day_21_team/Member_Data.java
@@ -9,12 +9,16 @@ public class Member_Data {
 	
 	public String getSerial() { return serial; }
 	public void setSerial(String serial) { this.serial = serial; }
+	
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
+	
 	public String getDepartment() { return department; }
 	public void setDepartment(String department) { this.department = department; }
+	
 	public String getPosition() { return position; }
 	public void setPosition(String position) { this.position = position; }
+	
 	public int getYear() { return year; }
 	public void setYear(int year) { this.year = year; }
 }
